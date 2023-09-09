0f4bb2083db952a27a128422401c5724c6835bef
Roushan <kroushan281@gmail.com>
2023-09-10 00:53:11 +0530

    first commit

---
+++ js/data.js
@@ -0,0 +1,20 @@
+let movies = [
+    {
+        image: 'img/banner/banner glau.jpg'
+    },
+    {
+        image: 'img/banner/TECH WORKSHOP.jpg'
+    },
+    {
+        image: 'img/banner/banner glau.jpg'
+    },
+    {
+        image: 'img/banner/banner4 DJ.jpg'
+    },
+    {
+        image: 'img/banner/banner glau.jpg'
+    },
+    {
+        image: 'img/banner/SPORTS.jpg'
+    }
+]
